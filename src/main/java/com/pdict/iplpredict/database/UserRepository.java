32999769565aa18d7b4142d2d29596cfaf6b5c78
kaiwatsatsat <kaiv.sathe@gmail.com>
2021-03-18 16:55:55 +0530

    User repository skeleton created

---
+++ src/main/java/com/pdict/iplpredict/database/UserRepository.java
@@ -0,0 +1,21 @@
+package com.pdict.iplpredict.database;
+
+import com.pdict.iplpredict.pojo.*;
+
+public class UserRepository {
+    public User getUserById(Integer userId) {
+        return null;
+    }
+
+    public void insertUser(User user) {
+
+    }
+
+    public void updateUser(User user) {
+
+    }
+
+    public void deleteUserById(Integer userId) {
+
+    }
+}
