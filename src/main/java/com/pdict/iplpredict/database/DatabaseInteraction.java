2df49af9ca9f53596d8f8d7d34f4903b80e107f6
kaiwatsatsat <kaiv.sathe@gmail.com>
2021-03-18 21:55:04 +0530

    created skeleton for DatabaseInteraction class

---
+++ src/main/java/com/pdict/iplpredict/database/DatabaseInteraction.java
@@ -0,0 +1,13 @@
+package com.pdict.iplpredict.database;
+
+import java.sql.ResultSet;
+
+public class DatabaseInteraction {
+    public ResultSet executeQuery(String sql) {
+        return null;
+    }
+
+    public void executeUpdate(String sql) {
+
+    }
+}
