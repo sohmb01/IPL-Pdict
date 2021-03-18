0d5b7f0924a3da482a4463642f068540664daad9
kaiwatsatsat <kaiv.sathe@gmail.com>
2021-03-18 23:59:04 +0530

    DatabaseInteraction implemented and UserRepository created

---
+++ src/main/java/com/pdict/iplpredict/database/ConenctionPool.java
@@ -0,0 +1,30 @@
+package com.pdict.iplpredict.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConenctionPool {
+    private static HikariDataSource ds;
+
+    static {
+        Properties props = new Properties();
+
+        props.setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
+        props.setProperty("dataSource.user", "postgres");
+        props.setProperty("dataSource.password", "123456");
+        props.setProperty("dataSource.databaseName", "postgres");
+
+        HikariConfig config = new HikariConfig(props);
+
+        ds = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+}
