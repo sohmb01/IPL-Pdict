8e754037e40dc186847b4e26d245e2251b84e7ff
kaiwatsatsat <kaiv.sathe@gmail.com>
2021-03-19 21:54:19 +0530

    created userservice

--- src/main/java/com/pdict/iplpredict/UserService.java
+++ src/main/java/com/pdict/iplpredict/UserService.java
@@ -1,1 +1,49 @@
-package com.pdict.iplpredict;  public class UserService { } 
+package com.pdict.iplpredict;
+
+import com.pdict.iplpredict.database.UserRepository;
+import com.pdict.iplpredict.entities.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.sql.SQLException;
+
+@Path("/user")
+public class UserService {
+    private UserRepository userRepository = new UserRepository();
+
+    @GET
+    @Path("/getUser/{username}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUser(@PathParam("username") String userName) throws SQLException {
+        User user = userRepository.getUserByUserName(userName);
+
+        return Response.ok()
+                .entity(user)
+                .build();
+    }
+
+    @POST
+    @Path("/createUser/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createUser(User user) throws SQLException {
+        userRepository.insertUser(user);
+        return Response.status(201).build();
+    }
+
+    @PUT
+    @Path("/updateUser")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateUser(User user) throws SQLException{
+        userRepository.updateUser(user);
+        return Response.status(201).build();
+    }
+
+    @DELETE
+    @Path("/deleteUser/{username}")
+    public Response deleteUser(String userName) throws SQLException {
+        userRepository.deleteUserByUserName(userName);
+        return Response.status(200).build();
+    }
+}
--- src/main/java/com/pdict/iplpredict/database/UserRepository.java
+++ src/main/java/com/pdict/iplpredict/database/UserRepository.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 public class UserRepository {
     DatabaseInteraction databaseInteraction = new DatabaseInteraction();
 
-    public User getUserByUsername(String username) throws SQLException {
-        String sql = "SELECT * FROM \"user\" WHERE username='" + username + "';";
+    public User getUserByUserName(String userName) throws SQLException {
+        String sql = "SELECT * FROM \"user\" WHERE userName='" + userName + "';";
 
         ResultSet resultSet = databaseInteraction.executeQuery(sql);
         resultSet.next();
@@ -19,23 +19,23 @@ public class UserRepository {
         String fullName = resultSet.getString("full_name");
         String favTeam = resultSet.getString("fav_team");
 
-        return new User(userId, username, password, fullName, favTeam);
+        return new User(userName, password, fullName, favTeam);
     }
 
-    public void insertUser(String username, String password, String fullName, String favTeam) throws SQLException {
-        String sql = "INSERT INTO \"user\" (username, password, full_name, fav_team) VALUES ('"+username+"', '"+password+"', '"+fullName+"', '"+favTeam+"');";
+    public void insertUser(User user) throws SQLException {
+        String sql = "INSERT INTO \"user\" (userName, password, full_name, fav_team) VALUES ('"+user.userName+"', '"+user.password+"', '"+user.fullName+"', '"+user.favTeam+"');";
 
         databaseInteraction.executeUpdate(sql);
     }
 
     public void updateUser(User user) throws SQLException {
-        String sql = "UPDATE \"user\" SET username='"+user.username+"', password='"+user.password+"', full_name='"+user.fullName+"', fav_team='"+user.favTeam+"' WHERE user_id="+user.userId+";";
+        String sql = "UPDATE \"user\" SET password='"+user.password+"', full_name='"+user.fullName+"', fav_team='"+user.favTeam+"' WHERE username='"+user.userName+"';";
 
         databaseInteraction.executeUpdate(sql);
     }
 
-    public void deleteUserById(Integer userId) throws SQLException {
-        String sql = "DELETE FROM \"user\" WHERE user_id="+userId+";";
+    public void deleteUserByUserName(String userName) throws SQLException {
+        String sql = "DELETE FROM \"user\" WHERE username='"+userName+"';";
 
         databaseInteraction.executeUpdate(sql);
     }
