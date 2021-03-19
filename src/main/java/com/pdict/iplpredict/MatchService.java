b51e6afe2ac4429a792dc421dc7ac14ec8c7eaa9
Roshan <roshan.rdx2@gmail.com>
2021-03-19 21:44:54 +0530

    match service, matchrepo and match pojo created

---
+++ src/main/java/com/pdict/iplpredict/MatchService.java
@@ -0,0 +1,57 @@
+package com.pdict.iplpredict;
+
+import com.pdict.iplpredict.database.MatchRepository;
+import com.pdict.iplpredict.entities.Match;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.sql.SQLException;
+import java.util.List;
+
+@Path("/match")
+public class MatchService {
+    private MatchRepository matchRepository = new MatchRepository();
+
+    @GET
+    @Path("/getMatch/{matchId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getMatch(@PathParam("id") Integer matchId) throws SQLException {
+        Match match = matchRepository.getMatchByMatchId(matchId);
+
+        return Response.ok()
+                .entity(match)
+                .build();
+    }
+
+    @GET
+    @Path("/getAllMatches")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllMatches() throws SQLException {
+        List<Match> matches = matchRepository.getAllMatches();
+
+        return Response.ok()
+                .entity(matches)
+                .build();
+    }
+
+    @POST
+    @Path("/createMatch/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createMatch(Match match) throws SQLException {
+        matchRepository.insertMatch(match);
+        return Response.status(201).build();
+    }
+
+    @PUT
+    @Path("/updateMatch")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateMatch(Match match) throws SQLException{
+        matchRepository.updateMatch(match);
+        return Response.status(201).build();
+    }
+
+
+}
+
---
+++ src/main/java/com/pdict/iplpredict/entities/Match.java
@@ -0,0 +1,24 @@
+package com.pdict.iplpredict.entities;
+
+public class Match {
+    public Integer matchId;
+    public Integer tournamentYear;
+    public String teamWin;
+    public String teamId1;
+    public String teamId2;
+    public Integer teamScore1;
+    public Integer teamScore2;
+    public Integer wickets;
+
+
+    public Match(Integer matchId, Integer tournamentYear, String teamWin, String teamId1, String teamId2, Integer teamScore1, Integer teamScore2, Integer wickets) {
+        this.matchId = matchId;
+        this.tournamentYear = tournamentYear;
+        this.teamWin = teamWin;
+        this.teamId1 = teamId1;
+        this.teamId2 = teamId2;
+        this.teamScore1 = teamScore1;
+        this.teamScore2 = teamScore2;
+        this.wickets = wickets;
+    }
+}
