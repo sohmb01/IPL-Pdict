8842592b0efc0042dc2ec0e0f09cb34a8c66abc5
roshan.rdx2 <roshan.rdx2@gmail.com>
2021-04-04 22:32:37 +0530

    added proper hrefs in users.html and written JS code in users.js

--- src/main/resources/webPageResources/user/user.js
+++ src/main/resources/webPageResources/user/user.js
@@ -18,3 +18,34 @@
       }, false)
     })
 })()
+
+
+document.getElementById('createUser').addEventListener('submit',createUser);
+
+function addPost(e){
+  e.preventDefault();
+
+
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+  let full_name = document.getElementById('fullName').value;
+  let fav_team = document.getElementById('team').value;
+
+//const data = { username: 'example' };
+
+    fetch('http://103.78.121.142:58080/iplpredict/user/createUser/', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username:username, password:password, full_name:full_name, fav_team:fav_team}),
+    })
+      .then(response => response.json())
+      .then(data => {
+          console.log('Success:', data);
+        })
+      .catch((error) => {
+          console.error('Error:', error);
+        })
+    };
