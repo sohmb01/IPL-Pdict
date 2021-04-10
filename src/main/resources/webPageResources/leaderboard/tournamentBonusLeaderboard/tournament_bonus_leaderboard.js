ee47b1dc369159e4287fd02ef758a8278f6b9868
roshan.rdx2 <roshan.rdx2@gmail.com>
2021-04-10 21:38:29 +0530

    bugfixes in tournament leaderboard js

--- src/main/resources/webPageResources/leaderboard/tournamentBonusLeaderboard/tournament_bonus_leaderboard.js
+++ src/main/resources/webPageResources/leaderboard/tournamentBonusLeaderboard/tournament_bonus_leaderboard.js
@@ -1,40 +1,43 @@
-/*
 
+let username = Cookies.get("HeaderUsername");
+let accesstoken = Cookies.get("AccessToken");
+if (!username || !accesstoken) {
+  bootbox.alert({
+    message: "Please Sign IN",
+  });
+
+  window.setTimeout(function () {
+    window.location.href =
+      "http://103.78.121.142:58080/iplpredict/landingPage/landing.html";
+  }, 1000);
+}
 
 fetch("http://103.78.121.142:58080/iplpredict/tournamentBonusLeaderboard/getTournamentBonusLeaderboard/2021",
-method: "GET", // or 'PUT'
-    headers: {
-      HeaderUsername: Cookies.get("HeaderUsername"),
-      AccessToken: Cookies.get("AccessToken"),
+    {
+        method: "GET", // or 'PUT'
+        headers: {
+        HeaderUsername: username,
+        AccessToken: accesstoken,
+      }
     }
-)
+  )
   .then((response) => response.json())
   .then((result) => {
-    console.log(result.data);
+    console.log("result",result);
+    console.log("result length",result.length);
+//    console.log("result body",result.body);
+//    console.log("result body length",result.body.length);
+
     // buildTable(result);
     var table = document.getElementById("myTable");
 
-    for (var i = 0; i < result.data.length; i++) {
+    for (var i = 0; i < result.length; i++) {
       var row = `<tr class="text-white">
-             <td>${result.data[i].email}</td>
-             <td>${result.data[i].first_name}</td>
+             <td>${result[i].username}</td>
+             <td>${result[i].matchPoints}</td>
            </tr>`;
       table.innerHTML += row;
     }
   });
 
-/*
-function buildTable(data) {
-  var table = document.getElementById("myTable");
 
-  for (var i = 0; i < data.length; i++) {
-    var row = `<tr>
-            <td>${data[i].email}</td>
-            <td>${data[i].first_name}</td>
-            <td>${data[i].last_name}</td>
-            <td>${data[i].avatar}</td>
-          </tr>`;
-    table.innerHTML += row;
-  }
-}
-*/
