3d8a60564916c64cdf1653285417c4ed86ef784a
roshan.rdx2 <roshan.rdx2@gmail.com>
2021-04-10 20:30:21 +0530

    current leaderboard js updated

--- src/main/resources/webPageResources/leaderboard/currentLeaderboard/current_leaderboard.js
+++ src/main/resources/webPageResources/leaderboard/currentLeaderboard/current_leaderboard.js
@@ -1,11 +1,24 @@
 
-/*
+let username = Cookies.get("HeaderUsername");
+let accesstoken = Cookies.get("AccessToken");
+if (!username || !accesstoken) {
+  bootbox.alert({
+    message: "Please Sign IN",
+  });
+
+  window.setTimeout(function () {
+    window.location.href =
+      "http://103.78.121.142:58080/iplpredict/landingPage/landing.html";
+  }, 1000);
+}
 
 fetch("http://103.78.121.142:58080/iplpredict/currentLeaderboard/getCurrentLeaderboard/2021",
   method: "GET", // or 'PUT'
       headers: {
          HeaderUsername: Cookies.get("HeaderUsername"),
         AccessToken: Cookies.get("AccessToken"),
+        HeaderUsername: username,
+        AccessToken: accesstoken,
       }
   )
   .then((response) => response.json())
@@ -16,13 +29,11 @@ fetch("http://103.78.121.142:58080/iplpredict/currentLeaderboard/getCurrentLeade
 
     for (var i = 0; i < result.data.length; i++) {
       var row = `<tr class="text-white">
-             <td>${result.data[i].email}</td>
-             <td>${result.data[i].first_name}</td>
+             <td>${result.data[i].username}</td>
+             <td>${result.data[i].matchPoints}</td>
            </tr>`;
       table.innerHTML += row;
     }
   });
 
 
-
-*/
